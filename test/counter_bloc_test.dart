153bdeb30b8baeb4cc41dcc6e3cd4e1158bde73c
Dima <dimaluka2014@gmail.com>
2024-10-09 18:41:54 +0300

    added a counter_bloc_test

---
+++ test/counter_bloc_test.dart
@@ -0,0 +1,37 @@
+import 'package:bloc_example/bloc/bloc/counter_bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CounterBloc', () {
+    CounterBloc? counterBloc;
+
+    setUp(() {
+      counterBloc = CounterBloc();
+    });
+    tearDown(() {
+      counterBloc?.close();
+    });
+
+    test('Checking the state CounterBloc (counter: 0)', () {
+      expect(CounterInitial(), isA<CounterState>());
+      expect(CounterInitial().counter, CounterState(counter: 0).counter);
+      debugPrint('State sanity check checked successfully');
+    });
+
+    blocTest(
+      'The bloc should emit a CounterState(counter: 0)',
+      build: () => CounterBloc(),
+      act: (bloc) => bloc.add(Increment()),
+      expect: () => [CounterState(counter: 1)],
+    );
+
+    blocTest(
+      'The bloc should emit a CounterState(counter: 0)',
+      build: () => CounterBloc(),
+      act: (bloc) => bloc.add(Decrement()),
+      expect: () => [CounterState(counter: -1)],
+    );
+  });
+}
