90bd677a98dff0d262c5ef37f4736d61b4e25055
Dima <dimaluka2014@gmail.com>
2024-10-09 18:42:25 +0300

    extends Equatable

--- lib/bloc/bloc/counter_bloc.dart
+++ lib/bloc/bloc/counter_bloc.dart
@@ -1,16 +1,17 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc():super(CounterInitial()){
-    on<Increment>((event, emit){
-      emit(CounterState(counter: state.counter+1));
+  CounterBloc() : super(CounterInitial()) {
+    on<Increment>((event, emit) {
+      emit(CounterState(counter: state.counter + 1));
     });
 
-    on<Decrement>((event, emit){
-      emit(CounterState(counter:state.counter-1));
+    on<Decrement>((event, emit) {
+      emit(CounterState(counter: state.counter - 1));
     });
   }
 }
--- lib/bloc/bloc/counter_state.dart
+++ lib/bloc/bloc/counter_state.dart
@@ -1,10 +1,15 @@
+// ignore_for_file: must_be_immutable
+
 part of 'counter_bloc.dart';
 
-class CounterState {
+class CounterState extends Equatable {
   int counter;
   CounterState({required this.counter});
+
+  @override
+  List<Object?> get props => [counter];
 }
 
 class CounterInitial extends CounterState {
-  CounterInitial():super(counter:0);
+  CounterInitial() : super(counter: 0);
 }
