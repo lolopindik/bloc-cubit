ae0cc007da11bf5f7235b62aa69869f1e5369052
Dima <dimaluka2014@gmail.com>
2024-10-11 01:45:23 +0300

    name changed due to mistake

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_example/logic/bloc/counter_bloc/counter_bloc.dart';
+import 'package:bloc_example/logic/presentation/screens/home_screen.dart';
 import 'package:bloc_example/logic/presentation/screens/second_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
