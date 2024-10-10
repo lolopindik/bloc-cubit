a6545b0e227869f04438b337dd7fc20dc0b5254d
Dima <dimaluka2014@gmail.com>
2024-10-11 01:53:42 +0300

    the main class was created for a more high-quality type of code

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,30 @@ import 'package:bloc_example/logic/presentation/screens/second_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  // ignore: no_leading_underscores_for_local_identifiers
+class Main {
   final CounterBloc _counterBloc = CounterBloc();
 
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.black12,
-          brightness: Brightness.dark,
+  void main() {
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.black12,
+            brightness: Brightness.dark,
+          ),
         ),
+        initialRoute: '/home',
+        routes: {
+          '/home': (context) => BlocProvider.value(
+              value: _counterBloc, child: const HomeScreen()),
+          '/second': (context) => BlocProvider.value(
+                value: _counterBloc,
+                child: const SecondScreen(),
+              )
+        },
       ),
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) =>
-            BlocProvider.value(value: _counterBloc, child: const HomeScreen()),
-        '/second': (context) => BlocProvider.value(
-              value: _counterBloc,
-              child: const SecondScreen(),
-            )
-      },
-    ),
-  );
+    );
+  }
 }
